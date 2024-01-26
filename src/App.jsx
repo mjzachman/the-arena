ba3e7036307bc7bd1b5a349ef78a3fdb54ae01ba
Matt Zachman <45126389+mjzachman@users.noreply.github.com>
2024-01-26 12:59:02 -0500

    Lay out homepage

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,67 @@ function App() {
 
   return (
     <>
-      <div>
+      <div class="nav-bar">
         <h1> THE ARENA </h1>
         <h3> Data Driven Sports Commentary </h3>
       </div>
 
-      <div>
-        <a>Article 1</a>
-        <a>Article 2</a>
-        <a>Article 3</a>
-        <a>Article 4</a>
-        <a>Article 5</a>
-        <a>Article 6</a>
-        <a>Article 7</a>
-        <a>Article 8</a>
-        <a>Article 9</a>
-        <a>Article 10</a>
+      <div class="card">
+        <h3>Bengals 2023 Season in Review</h3>
+        <p>text text text text text text text text text</p>
       </div>
 
+      <div class="card">
+        <h3>Wildcats Basketball Postseason Preview</h3>
+        <p>text text text text text text text text text</p>
+      </div>
+
+      <div class="card">
+        <h3>2023 NFL Mock Draft</h3>
+        <p>text text text text text text text text text</p>
+      </div>
+
+      <div class="card">
+        <h3>Bengals Offseason Moves</h3>
+        <p>text text text text text text text text text</p>
+      </div>
+
+      <div class="card">
+        <h3>Ranking all 2023 Quarterbacks</h3>
+        <p>text text text text text text text text text</p>
+      </div>
+
+      <div class="card">
+        <h3>SEC Football in 2024</h3>
+        <p>text text text text text text text text text</p>
+      </div>
+
+      <div class="card">
+        <h3>Top 5 NFL Front Offices</h3>
+        <p>text text text text text text text text text</p>
+      </div>
+
+      <div class="card">
+        <h3>The Case for LeBron as the G.O.A.T.</h3>
+        <p>text text text text text text text text text</p>
+      </div>
+
+      <div class="card">
+        <h3>NFL International</h3>
+        <p>text text text text text text text text text</p>
+      </div>
+
+      <div class="card">
+        <h3>Why the Reds will win the divison this year</h3>
+        <p>text text text text text text text text text</p>
+      </div>
+
+      <div class="footer">
+        <h3>About Me</h3>
+        <p>text text text text text text text text text</p>
+      </div>
+
+      
     </>
   )
 }
